3bcbda140243219dd0a1eddf786cec9f9d070e4e
Jacob Goh Chuan Ching <jacobgoh101@gmail.com>
2017-02-12 18:08:53 +0800

    basically done

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const locale = require("locale");
+// const os = require('os');
+var uaParser = require('ua-parser-js');
+
+const app = express();
+
+app.get('*', (req,res) => {
+  res.setHeader("Content-Type", "application/json");
+
+  const locales = new locale.Locales(req.headers["accept-language"]);
+  let ipaddress = req.ip;
+  let language = locales[0];
+  let software = uaParser(req.headers["user-agent"]);
+  software = software.os;
+  let result = {
+    ipaddress,
+    language,
+    software
+  };
+  result = JSON.stringify(result);
+  res.end(result);
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port);
